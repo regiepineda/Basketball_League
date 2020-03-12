d5d69e4b2b08d04b8486e87ba0fe12d97d6bd9df
Cody Rhee <crhee94@uw.edu>
2020-03-12 15:00:11 -0700

    Connecting postgreSQL and testing SQL commands

--- app.py
+++ app.py
@@ -4,6 +4,117 @@ from psycopg2 import Error
 
 app = Flask(__name__)
 
+def connectToDB():
+    try:
+        return psycopg2.connect(host = "localhost", port = "5432", database = "test")
+    except (Exception, psycopg2.Error) as error:
+        print("Can't connect to database")
+        print(error)
+
+def createTables(cursor):
+    try:
+        cursor.execute(
+            ''' CREATE TABLE IF NOT EXISTS "Team" (
+                    ID           SERIAL NOT NULL,
+                    TeamName     VARCHAR(100) NOT NULL UNIQUE,
+
+                    PRIMARY KEY (ID)
+            );'''
+        )
+        print("Team: Works")
+
+        cursor.execute(
+            ''' CREATE TABLE IF NOT EXISTS "Player" (
+                    ID           SERIAL NOT NULL,
+                    FirstName    VARCHAR(30) NOT NULL,
+                    LastName     VARCHAR(30) NOT NULL,
+                    Height       VARCHAR(5),
+                    Weight       INT,
+                    Age          INT,
+                    TeamId       INT NOT NULL,
+                    
+                    PRIMARY KEY (ID),
+                    FOREIGN KEY (TeamId) REFERENCES "Team" (Id)
+            );'''
+        )
+        print("Player: Works")
+
+        cursor.execute(
+            ''' CREATE TABLE IF NOT EXISTS "Season" (
+                    Year         INT UNIQUE,
+
+                    PRIMARY KEY (YEAR)
+            ); '''
+        )
+        print("Season: Works")
+
+        cursor.execute(
+            ''' CREATE TABLE IF NOT EXISTS "Game" (
+                    ID           SERIAL NOT NULL,
+                    SeasonYear   INT NOT NULL,
+                    HomeTeamId   INT NOT NULL,
+                    AwayTeamId   INT NOT NULL,
+                    WinnerId     INT,
+                    LoserId      INT,
+                    GameDate	 DATE,	
+
+                    PRIMARY KEY (ID),
+                    FOREIGN KEY (SeasonYear) REFERENCES "Season" (Year),
+                    FOREIGN KEY (HomeTeamId) REFERENCES "Team" (Id),
+                    FOREIGN KEY (AwayTeamId) REFERENCES "Team" (Id)
+            ); '''
+        )
+        print("Game: Works")
+
+        cursor.execute(
+            ''' CREATE TABLE IF NOT EXISTS "StatType" (
+                    ID           VARCHAR(5) NOT NULL,
+                    Description  VARCHAR(30),
+
+                    PRIMARY KEY (ID)
+            );'''
+        )
+        print("StatType: Works")
+
+        cursor.execute(
+            ''' CREATE TABLE IF NOT EXISTS "StatInfo" (
+                    ID           SERIAL NOT NULL,
+                    PlayerID     INT NOT NULL,
+                    GameID       INT NOT NULL,
+                    Type         VARCHAR(5) NOT NULL,
+                    StatValue    INT NOT NULL,
+                    
+                    PRIMARY KEY (ID),
+                    FOREIGN KEY (PlayerId) REFERENCES "Player" (Id),
+                    FOREIGN KEY (GameId) REFERENCES "Game" (Id),
+                    FOREIGN KEY (Type) REFERENCES "StatType" (Id)
+            );'''
+        )
+        print("StatInfo: Works")
+
+        cursor.execute(
+            ''' CREATE TABLE IF NOT EXISTS "TeamInSeason" (
+                    SeasonId     INT NOT NULL,
+                    TeamId       INT NOT NULL,
+                    
+                    PRIMARY KEY (SeasonId, TeamId),
+                    FOREIGN KEY (SeasonId) REFERENCES "Season" (Year),
+                    FOREIGN KEY (TeamId) REFERENCES "Team" (Id)
+            );'''
+        )
+        print("TeamInSeason: Works")
+        db.commit()
+    except (Exception, psycopg2.DatabaseError) as error :
+        print ("Error while creating PostgreSQL table", error)
+        db.rollback()
+    finally:
+        #closing database connection.
+            if(db):
+                cursor.close()
+                db.close()
+                print("PostgreSQL connection is closed")
+
+"""
 try:
     db = psycopg2.connect(
             host = "localhost",
@@ -12,118 +123,28 @@ try:
     )
 
     cursor = db.cursor()
-    #cursor.execute(open("initial_adds.sql", )
-    
-    """
-    cursor.execute(
-        ''' CREATE TABLE IF NOT EXISTS "Team" (
-                ID           SERIAL NOT NULL,
-                TeamName     VARCHAR(100) NOT NULL UNIQUE,
+ """   
 
-                PRIMARY KEY (ID)
-        );'''
-    )
-    print("Team: Works")
-
-    cursor.execute(
-        ''' CREATE TABLE IF NOT EXISTS "Player" (
-                ID           SERIAL NOT NULL PRIMARY KEY,
-                FirstName    VARCHAR[30] NOT NULL,
-                LastName     VARCHAR[30] NOT NULL,
-                Height       INT NOT NULL,
-                Weight       INT NOT NULL,
-                Age          INT NOT NULL,
-                TeamId       INT NOT NULL,
-                
-                FOREIGN KEY (TeamId) REFERENCES "Team" (Id)
-        );'''
-    )
-    print("Player: Works")
-
-    cursor.execute(
-        ''' CREATE TABLE IF NOT EXISTS "Season" (
-                Year         INT PRIMARY KEY
-        ); '''
-    )
-    print("Season: Works")
-
-    cursor.execute(
-        ''' CREATE TABLE IF NOT EXISTS "Game" (
-                ID           SERIAL NOT NULL PRIMARY KEY,
-	            SeasonYear   INT NOT NULL,
-	            HomeTeamId   INT NOT NULL,
-	            AwayTeamID   INT NOT NULL,
-	            Result       VARCHAR[10],
-	            GameDate	 DATE,	
-	            FOREIGN KEY (SeasonYear) REFERENCES "Season" (Year),
-	            FOREIGN KEY (HomeTeamId) REFERENCES "Team" (Id),
-	            FOREIGN KEY (AwayTeamId) REFERENCES "Team" (Id)
-        ); '''
-    )
-    print("Game: Works")
-
-    cursor.execute(
-        ''' CREATE TABLE IF NOT EXISTS "StatType" (
-                ID           VARCHAR[5] NOT NULL PRIMARY KEY,
-                Description  VARCHAR[30]
-        );'''
-    )
-    print("StatType: Works")
-
-    cursor.execute(
-        ''' CREATE TABLE IF NOT EXISTS "StatInfo" (
-                ID           SERIAL NOT NULL PRIMARY KEY,
-                StatValue    INT NOT NULL,
-                PlayerID     INT NOT NULL,
-                GameID       INT NOT NULL,
-                Type         VARCHAR[5] NOT NULL,
-                
-                FOREIGN KEY (PlayerId) REFERENCES "Player" (Id),
-                FOREIGN KEY (GameId) REFERENCES "Game" (Id),
-                FOREIGN KEY (Type) REFERENCES "StatType" (Id)
-        );'''
-    )
-    print("StatInfo: Works")
-
-    cursor.execute(
-        ''' CREATE TABLE IF NOT EXISTS "TeamInSeason" (
-                SeasonId     INT NOT NULL,
-                TeamId       INT NOT NULL,
-                
-                PRIMARY KEY (SeasonId, TeamId),
-                FOREIGN KEY (SeasonId) REFERENCES "Season" (Year),
-                FOREIGN KEY (TeamId) REFERENCES "Team" (Id)
-        );'''
-    )
-    print("TeamInSeason: Works")
-    """
-
-    postgres_insert_query = '''INSERT INTO "Team" (TeamName) VALUES
-                  (%s),
-                  (%s),
-                  (%s),
-                  (%s),
-                  (%s),
-                  (%s);'''
-
-    record_to_insert = ('Team1', 'Team2', 'Team3', 'Team4', 'Team5', 'Team6')
-
-    cursor.execute(postgres_insert_query, record_to_insert) 
-
-    db.commit()
-except (Exception, psycopg2.DatabaseError) as error :
-    print ("Error while creating PostgreSQL table", error)
-    db.rollback()
-finally:
-    #closing database connection.
-        if(db):
-            cursor.close()
-            db.close()
-            print("PostgreSQL connection is closed")
+db = connectToDB()
+cursor = db.cursor()
+#createTables(cursor)
 
 @app.route('/')
 def home():
-    return "Hello World!"
+    try:
+        cursor.execute(
+            ''' SELECT FirstName, LastName, Age, Height, Weight, TeamName
+                FROM "Player"
+                     INNER JOIN "Team" ON ( "Team".Id = "Player".TeamId );
+            '''
+        )
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        
+    result = cursor.fetchall()
+    for row in result:
+        print(result)
+    return render_template("home.html", results = result)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- initial_players.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO Team (TeamName) VALUES
-       ("Team1"),
-       ("Team2"),
-       ("Team3"),
-       ("Team4"),
-       ("Team5"),
-       ("Team6");
