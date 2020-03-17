aaa91e46388a353bef231f3750da3fdb9e44d283
Cody Rhee <crhee94@uw.edu>
2020-03-16 19:33:41 -0700

    game_stats table demo

--- app.py
+++ app.py
@@ -116,18 +116,89 @@ def createTables(cursor):
                 # db.close()
                 print("PostgreSQL connection is closed") 
 
-def queryTeam(game):
+def queryHomeTeam(game):
     cursor.execute(
-        ''' 
-        SELECT FirstName, LastName, TeamName, Type, StatValue
-        FROM "StatInfo"
-             INNER JOIN "Player" ON ( "StatInfo".PlayerId = "Player".Id )
-             INNER JOIN "Team" ON ( "Player".TeamId = "Team".Id )
-        WHERE gameid = %s
-        ORDER BY TeamName, "Team".Id, "Player".Id;
+        '''
+        SELECT "C".FirstName, "C".LastName, "C".TeamName, "A".Points, "B".Rebound, "C".Assist
+        FROM (
+            SELECT "Player".Id AS PlayerId, "Game".Id AS GameId, FirstName, LastName, "Team".TeamName AS TeamName, StatValue AS Points
+            FROM "Player"
+                 INNER JOIN "Team" ON ( "Player".TeamId = "Team".Id )
+	             INNER JOIN "StatInfo" ON ( "Player".Id = "StatInfo".PlayerId )
+                 INNER JOIN "Game" ON ( "Team".Id = "Game".HomeTeamId AND "StatInfo".GameId = "Game".Id )
+            WHERE "StatInfo".Type = 'Pts'
+            ) AS "A"
+            INNER JOIN (
+                SELECT "Player".Id AS PlayerId, "Game".Id AS GameID, FirstName, LastName, "Team".TeamName AS TeamName, StatValue AS Rebound
+                FROM "Player"
+                     INNER JOIN "Team" ON ( "Player".TeamId = "Team".Id )
+	                 INNER JOIN "StatInfo" ON ( "Player".Id = "StatInfo".PlayerId )
+	                 INNER JOIN "Game" ON ( "Team".Id = "Game".HomeTeamId AND "StatInfo".GameId = "Game".Id )
+                WHERE "StatInfo".Type = 'Reb'
+            ) AS "B"
+            ON "A".PlayerId = "B".PlayerId
+            INNER JOIN (
+                SELECT "Player".Id AS PlayerId, "Game".Id AS GameID, FirstName, LastName, "Team".TeamName AS TeamName, StatValue AS Assist
+                FROM "Player"
+                     INNER JOIN "Team" ON ( "Player".TeamId = "Team".Id )
+	                 INNER JOIN "StatInfo" ON ( "Player".Id = "StatInfo".PlayerId )
+	                 INNER JOIN "Game" ON ( "Team".Id = "Game".HomeTeamId AND "StatInfo".GameId = "Game".Id )
+                WHERE "StatInfo".Type = 'Ast'
+            ) AS "C"
+            ON "B".PlayerId = "C".PlayerId
+        WHERE "C".GameId = %s;
         ''', (game[4:]))
     rows = cursor.fetchall()
+    return rows
 
+def queryAwayTeam(game):
+    cursor.execute(
+        '''
+        SELECT "C".FirstName, "C".LastName, "C".TeamName, "A".Points, "B".Rebound, "C".Assist
+        FROM (
+            SELECT "Player".Id AS PlayerId, "Game".Id AS GameId, FirstName, LastName, "Team".TeamName AS TeamName, StatValue AS Points
+            FROM "Player"
+                 INNER JOIN "Team" ON ( "Player".TeamId = "Team".Id )
+	             INNER JOIN "StatInfo" ON ( "Player".Id = "StatInfo".PlayerId )
+                 INNER JOIN "Game" ON ( "Team".Id = "Game".AwayTeamId AND "StatInfo".GameId = "Game".Id )
+            WHERE "StatInfo".Type = 'Pts'
+            ) AS "A"
+            INNER JOIN (
+                SELECT "Player".Id AS PlayerId, "Game".Id AS GameID, FirstName, LastName, "Team".TeamName AS TeamName, StatValue AS Rebound
+                FROM "Player"
+                     INNER JOIN "Team" ON ( "Player".TeamId = "Team".Id )
+	                 INNER JOIN "StatInfo" ON ( "Player".Id = "StatInfo".PlayerId )
+	                 INNER JOIN "Game" ON ( "Team".Id = "Game".AwayTeamId AND "StatInfo".GameId = "Game".Id )
+                WHERE "StatInfo".Type = 'Reb'
+            ) AS "B"
+            ON "A".PlayerId = "B".PlayerId
+            INNER JOIN (
+                SELECT "Player".Id AS PlayerId, "Game".Id AS GameID, FirstName, LastName, "Team".TeamName AS TeamName, StatValue AS Assist
+                FROM "Player"
+                     INNER JOIN "Team" ON ( "Player".TeamId = "Team".Id )
+	                 INNER JOIN "StatInfo" ON ( "Player".Id = "StatInfo".PlayerId )
+	                 INNER JOIN "Game" ON ( "Team".Id = "Game".AwayTeamId AND "StatInfo".GameId = "Game".Id )
+                WHERE "StatInfo".Type = 'Ast'
+            ) AS "C"
+            ON "B".PlayerId = "C".PlayerId
+        WHERE "C".GameId = %s;
+        ''', (game[4:]))
+    rows = cursor.fetchall()
+    return rows
+
+def queryGameResult(game):
+    cursor.execute(
+        '''
+        SELECT "Team".TeamName AS TeamName, SUM(StatValue) AS Points
+        FROM "Player"
+             INNER JOIN "Team" ON ( "Player".TeamId = "Team".Id )
+            INNER JOIN "StatInfo" ON ( "Player".Id = "StatInfo".PlayerId )
+            INNER JOIN "Game" ON ( ("Team".Id = "Game".HomeTeamId AND "StatInfo".GameId = "Game".Id) 
+						   OR  ("Team".Id = "Game".AwayTeamId AND "StatInfo".GameId = "Game".Id) )
+        WHERE "StatInfo".Type = 'Pts' AND "Game".Id = %s
+        GROUP BY "Team".Id;
+        ''', (game[4:]))
+    rows = cursor.fetchall()
     return rows
 
 """
@@ -178,14 +249,16 @@ def game():
     if request.method == "POST":
         game = request.form['game']
         print(game)
-        teams = queryTeam(game)
-        if teams is not None:
-            return render_template("game_stats.html", title='Game', teams=teams)
+        homeTeam = queryHomeTeam(game)
+        awayTeam = queryAwayTeam(game)
+        gameResult = queryGameResult(game)
+        if homeTeam and awayTeam is not None:
+            return render_template("game_stats.html", title='Game', homeTeam=homeTeam, 
+                   awayTeam=awayTeam, gameResult=gameResult)
         else:
             gameInfo = gamePreview(game)
             print(gameInfo)
             return render_template("game_stats.html", title='Game', gameInfo = gameInfo)
-            
     return render_template("game_stats.html", title='Game')
 
 @app.route('/standing')
